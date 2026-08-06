31bad125512aa2d1ea58c52ca941dbd3cb34fc09
Z-Corp (Z-Sys company) <135663208+ZSys-bluecombOS@users.noreply.github.com>
2026-08-06 14:15:37 -0400

    Update MtxColor.py

--- ''reference version'' - Python v2/MtxColor.py
+++ ''reference version'' - Python v2/MtxColor.py
@@ -7,6 +7,16 @@ class MtxColor_base:
 
     self.alpha = alpha
 
+
+
 class RGB(MtxColor_base):
   def __init__(self, colors, alpha):
     super().__init__(colors, alpha, 3)
+
+
+
+def from_rgb_hex(code):
+  if code[0] == "#":
+    code = code[1:]
+
+  return RGB([int(code[:2], 16)/255, int(code[2:4], 16)/255, int(code[4:6], 16)/255], int(code[6:8], 16)/255 if len(code) == 8 else 255)
