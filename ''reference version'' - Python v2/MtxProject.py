ef9386dcac4af53011e7e65f1c3e0d0ed32211d1
Z-Corp (Z-Sys company) <135663208+ZSys-bluecombOS@users.noreply.github.com>
2026-08-26 17:18:08 -0400

    Update MtxProject.py
    Notes

---
+++ ''reference version'' - Python v2/MtxProject.py
@@ -0,0 +1,5 @@
+# Project format:
+# Basically just folder
+# However, ".mtxasset" is for project stuff
+# Like libraries (".mtxasset/lib") and project commands (".mtxasset/cmd")
+# Unsure how sub-projects will be handled still
