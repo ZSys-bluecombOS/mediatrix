1d6d9fa32daffe5142c629cb5027de0616525385
Z-Corp (Z-Sys company) <135663208+ZSys-bluecombOS@users.noreply.github.com>
2026-09-12 17:25:30 -0400

    Update MtxProject.py

--- ''reference version'' - Python v2/MtxProject.py
+++ ''reference version'' - Python v2/MtxProject.py
@@ -3,3 +3,5 @@
 # However, ".mtxasset" is for project stuff
 # Like libraries (".mtxasset/lib") and project commands (".mtxasset/cmd")
 # Unsure how sub-projects will be handled still
+
+# Edit: okay there will be way more subfolders for that most likely
